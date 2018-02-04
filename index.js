066dd926d7a3e6471702b8c18613ee8384695b4f
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2018-02-04 20:47:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Rhythm Guitar", "Bass", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(members, inst) {
+  var empty_array = []
+  for (i = 0; i < members.length; i ++) {
+    empty_array[i] = `${members[i]} plays ${inst[i]}`
+  }
+}
